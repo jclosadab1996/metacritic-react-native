63039ed9c6b33a3a8a5e9d29f20d74cbef050df8
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2024-09-07 01:34:31 -0500

    inclusion de tailwind y configuracion

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { StyleSheet, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { Main } from "./components/Main";
-import { Logo } from "./components/Logo";
 
 export default function App() {
   return (
